a4bcacc195ad5a616c74813692918fd8c0969a9e
Emilio Carrión <emcaryp@gmail.com>
2025-11-04 00:07:19 +0100

    Changes

--- views/output/application.py
+++ views/output/application.py
@@ -3,14 +3,31 @@ from client import DittoClient
 from model import Truck
 
 
+from model import Coordinate, Polygon, String
 
 ditto_client = DittoClient()
 
 
-def get_trucks_by_brand(brand: str) -> list[Truck]:
+def get_all_trucks_inside_zone(zone: Polygon) -> list[Truck]:
     entities = ditto_client.query(Truck)
     matching_entities = []
     for entity in entities:
-        if entity.get_brand() == brand:
+        if entity.get_location().within(zone):
+            matching_entities.append(entity)
+    return matching_entities
+
+def get_all_trucks_near_place(place: Coordinate) -> list[Truck]:
+    entities = ditto_client.query(Truck)
+    matching_entities = []
+    for entity in entities:
+        if entity.get_location().withinDistance(200, place):
+            matching_entities.append(entity)
+    return matching_entities
+
+def get_all_trucks_with_license_plate_ending(ending: String) -> list[Truck]:
+    entities = ditto_client.query(Truck)
+    matching_entities = []
+    for entity in entities:
+        if entity.get_license_plate().endsWith(ending):
             matching_entities.append(entity)
     return matching_entities
--- views/output/model.py
+++ views/output/model.py
@@ -1,6 +1,37 @@
 from dataclasses import dataclass
+from shapely.geometry import Point, Polygon as ShapelyPolygon
 
 ### Value Sets
+
+
+@dataclass
+class Coordinate:
+    latitude: float
+    longitude: float
+
+    def within(self, polygon: "Polygon") -> bool:
+        point = Point(self.longitude, self.latitude)
+        return point.within(
+            ShapelyPolygon([(c.longitude, c.latitude) for c in polygon.coordinates])
+        )
+
+    def withinDistance(self, distance: float, other: "Coordinate") -> bool:
+        point1 = Point(self.longitude, self.latitude)
+        point2 = Point(other.longitude, other.latitude)
+        return point1.distance(point2) <= distance
+
+
+@dataclass
+class Polygon:
+    coordinates: list[Coordinate]
+
+
+@dataclass
+class String:
+    value: str
+
+    def endsWith(self, ending: str) -> bool:
+        return self.value.endswith(ending)
 
 
 ### Entities
@@ -9,16 +40,28 @@ from dataclasses import dataclass
 @dataclass
 class Truck:
     thing_id: str
-    brand: str
-    serial_number: str
+    location: Coordinate
+    license_plate: String
 
     @classmethod
     def from_ditto(cls, ditto_entity) -> "Truck":
         return cls(
             thing_id=ditto_entity["thingId"],
-            brand=ditto_entity["attributes"]["brand"],
-            serial_number=ditto_entity["attributes"]["serialno"],
+            location=Coordinate(
+                latitude=ditto_entity["attributes"]["Location"]["latitude"],
+                longitude=ditto_entity["attributes"]["Location"]["longitude"],
+            ),
+            license_plate=String(value=ditto_entity["attributes"]["License Plate"]),
         )
 
-    def get_brand(self):
-        return self.brand
+    def get_location(self):
+        """
+        Implement this method to handle the Get Location interface.
+        """
+        return self.location
+
+    def get_license_plate(self):
+        """
+        Implement this method to handle the Get License Plate interface.
+        """
+        return self.license_plate
